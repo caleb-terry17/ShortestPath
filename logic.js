a0f340a686245fc164648570b19d33f5b26925ca
caleb-terry17 <k.cterry30@gmail.com>
2022-03-16 14:23:08 -0500

    completed table rendering and button color selection

--- logic.js
+++ logic.js
@@ -2,10 +2,11 @@
 // globals
 ///////////////////
 
-table = document.getElementById("table");
+let table = document.getElementById("table");  // table to dispaly search
+let actionColor = "#E70000";  // starts out as red
 
 ///////////////////
-// i/o
+// table
 ///////////////////
 // renders the table the user will see
 function renderTable() {
@@ -17,11 +18,35 @@ function renderTable() {
         let tr = document.createElement('tr');
         // creating each column in the row
         for (let col = 0; col < cols; ++col) {
-            tr.appendChild(document.createElement('td'));
+            // creating tags for row, col index of table
+            let button = document.createElement('button');
+            let td = document.createElement('td');
+            // adding click event to button
+            button.onclick = function () { return select(row, col) };
+            // adding tags to table  
+            td.appendChild(button);
+            tr.appendChild(td);
         }
         // appending row to table
         table.appendChild(tr);
     }
 }
+
+// changes the color of the square selected to the "action" color
+function select(row, col) {
+    let button = table.children[row].children[col].children[0];  // button the user selected
+    // "&nbsp;" tells me that the button's color is currently set to the "action" color
+    if (button.innerHTML == "&nbsp;") {  // button is already action color, revert to white
+        button.style.background = "#FFF";
+        button.innerHTML = "";
+    } else {  // otherwise => change to action color
+        button.style.background = actionColor;
+        button.innerHTML = "&nbsp;";
+    }
+}
+
+///////////////////
+// shortest path
+///////////////////
 
 function computeSP() {}
